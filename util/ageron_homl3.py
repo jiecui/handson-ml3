8586a3ed98c27a12976bd5b8de843d77b24d5365
Richard Jie Cui <richard.cui@utoronto.ca>
2025-01-21 23:18:08 -0600

    chore: Update and add image folder name

--- util/ageron_homl3.py
+++ util/ageron_homl3.py
@@ -2,7 +2,7 @@
 Library of A Geron, Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow, 3rd Edition
 
 Copyright 2021-2025 Richard J. Cui Created: Mon 01/11/2021  3:21:04.437 PM
-Revision: 0.3  Date: Sun 01/12/2025 10:46:10.016570 AM
+Revision: 0.4  Date: Tue 01/21/2025 23:14:52.692687 PM
 
 Rocky Creek Dr. NE
 Rochester, MN 55906, USA
@@ -13,23 +13,23 @@ Email: richard.jie.cui@gmail.com
 # ==========================================================================
 # Constants
 # ==========================================================================
-DOWNLOAD_ROOT_OLD = "http://raw.githubusercontent.com/ageron/handson-ml2/master/" # 2nd Edition
-DOWNLOAD_ROOT = "https://github.com/ageron/data/raw/main/" # 3rd Edition
+from sklearn.datasets import fetch_openml
+import joblib
+import pandas as pd
+import urllib.request
+import matplotlib.pyplot as plt
+import shutil
+import os
+import sklearn
+import sys
+import tarfile
+DOWNLOAD_ROOT_OLD = "http://raw.githubusercontent.com/ageron/handson-ml2/master/"  # 2nd Edition
+DOWNLOAD_ROOT = "https://github.com/ageron/data/raw/main/"  # 3rd Edition
 HOML3_ROOT = "https://github.com/ageron/handson-ml3/raw/main/"
 
 # ==========================================================================
 # Libraries
 # ==========================================================================
-import tarfile
-import sys
-import sklearn
-import os
-import shutil
-import matplotlib.pyplot as plt
-import urllib.request
-import pandas as pd
-import joblib
-from sklearn.datasets import fetch_openml
 
 # check system requirements
 # Python ≥ 3.5 is required
@@ -41,6 +41,8 @@ assert sklearn.__version__ >= "0.20"
 # Global
 # ==========================================================================
 # get image root path
+
+
 def get_image_root():
     '''Get image root path'''
 
@@ -49,6 +51,8 @@ def get_image_root():
     return os.path.abspath(images_path)
 
 # get data root path
+
+
 def get_data_root():
     '''Get data root path'''
 
@@ -57,6 +61,8 @@ def get_data_root():
     return os.path.abspath(data_path)
 
 # Create a function to save the figures.
+
+
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300,
              images_folder="images"):
     '''Save the figure'''
@@ -73,6 +79,8 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300,
 # Chapter 1
 # ==========================================================================
 # Chapter 1: Prepare country satisfaction data
+
+
 def prepare_country_stats(oecd_bli, gdp_per_capita):
     '''PREPARE_COUNTRY_STATS function just merges the OECD's life
     satisfaction data and the IMF's GDP per capita data. It's a bit too long
@@ -97,14 +105,18 @@ def prepare_country_stats(oecd_bli, gdp_per_capita):
     return sample_data, missing_data
 
 # Chapter 1: Load life satisfaction data
+
+
 def load_lifesat():
     '''Load life satisfaction data for Chapter 1'''
 
-    data_root=get_data_root()
+    data_root = get_data_root()
     csv_path = os.path.join(data_root, "lifesat", "lifesat.csv")
     return pd.read_csv(csv_path)
 
 # Chapter 1: Download life satisfaction data
+
+
 def download_lifesat():
     '''Download life satisfaction data for Chapter 1
 
@@ -132,6 +144,8 @@ def download_lifesat():
 # Chapter 2
 # ==========================================================================
 # Chapter 2: Load California housing data
+
+
 def load_housing_data():
     '''Load California housing data'''
 
@@ -140,6 +154,8 @@ def load_housing_data():
     return pd.read_csv(csv_path)
 
 # Chapter 2: Download California housing data
+
+
 def download_housing_data():
     '''Dowload California housing data'''
 
@@ -158,16 +174,18 @@ def download_housing_data():
         housing_tgz.extractall(path=data_root)
     # move the extracted file to the datapath, overwrite it if already exists
     try:
-        destination_file = os.path.join(datapath,filename)
+        destination_file = os.path.join(datapath, filename)
         if os.path.exists(destination_file):
             print(f"Removing existing file '{destination_file}'")
-            os.remove(destination_file) 
+            os.remove(destination_file)
         shutil.move(tarball_path, datapath)
         print(f"File '{tarball_path}' moved successfully to '{datapath}'.")
     except Exception as e:
         print(f"Error moving file: {e}")
 
 # Chapter 2: Read California image
+
+
 def read_california_image():
     '''Read California image'''
 
@@ -179,17 +197,19 @@ def read_california_image():
     # download the image if it doesn't exist
     if not os.path.exists(image_file):
         download_california_image()
-        
+
     return plt.imread(image_file)
 
 # Chapter 2: Download California image
+
+
 def download_california_image():
     '''Download California image'''
 
     image_root = get_image_root()
     imagepath = os.path.join(image_root, "end_to_end_project")
     os.makedirs(imagepath, exist_ok=True)
-    
+
     filename = "california.png"
     print("Downloading", filename)
     url = HOML3_ROOT+"images/end_to_end_project/"+filename
@@ -199,6 +219,8 @@ def download_california_image():
 # Chapter 3
 # ==========================================================================
 # Chapter 3: Load MNIST data
+
+
 def load_mnist_data():
     '''Load MNIST data'''
 
@@ -214,6 +236,8 @@ def load_mnist_data():
     return joblib.load(mnist_file)
 
 # Chapter 3: Download MNIST data
+
+
 def download_mnist_data():
     '''Download MNIST data'''
 
@@ -226,5 +250,5 @@ def download_mnist_data():
     minst_file = os.path.join(datapath, f"{filename}.joblib")
     mnist = fetch_openml(filename, version=1, as_frame=False, parser='pandas')
     joblib.dump(mnist, minst_file)
-                               
+
 # [EOF]
